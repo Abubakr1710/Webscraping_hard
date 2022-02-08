671b08d89fb722ceb3410863b0bce00829cf3520
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-02-08 13:54:45 +0500

    Update hard.py

--- hard.py
+++ hard.py
@@ -1,1 +1,21 @@
-print('Hello')
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+page = requests.get("https://weather.com/weather/tenday/l/San+Francisco+CA?canonicalCityId=dfdaba8cbe3a4d12a8796e1f7b1ccc7174b4b0a2d5ddb1c8566ae9f154fa638c")
+soup =BeautifulSoup(page.content, 'html.parser')
+
+list_day = []
+list_desc = []
+list_min_temp = []
+list_max_temp = []
+
+#Getting Days
+#--------------------------------------------------------
+
+days = soup.find_all('h2', class_ = "DetailsSummary--daypartName--2FBp2")[1:11:1]
+for d in days:
+    day_get = d.text
+    list_day.append(day_get)
+
+print(list_day)
